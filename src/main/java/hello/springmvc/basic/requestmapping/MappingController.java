65283ca21cc04949cefbf6bddaa9e2b5a65ddbca
parksangwoo <b01056721617@gmail.com>
2021-07-20 15:22:14 +0900

    📑 :: 요청 매핑 로직

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -1,2 +1,44 @@
-package hello.springmvc.basic.requestmapping;public class MappingController {
+package hello.springmvc.basic.requestmapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class MappingController {
+
+    @GetMapping("/hello-basic")
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable String userId) {
+        log.info("mappingPath userId={}", userId);
+        return "ok";
+    }
+
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath2(@PathVariable String userId, @PathVariable Long orderId) {
+        log.info("mappingPath userid={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-param", params = "mode=debug")
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-header", headers = "mode=debug")
+    public String mappingHeader() {
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+
 }
